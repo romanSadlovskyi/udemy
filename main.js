7810c7dfc9e38926bb3723ffbf6bc61111f2c25a
romanSadlovskyi <poloskyn19@gmail.com>
2021-03-14 18:10:31 +0200

    Second practice task

--- main.js
+++ main.js
@@ -8,8 +8,53 @@ const personalMovieDB = {
     privat: false
 };
 
-const lastFilm = prompt('Який був ваш один з останніх переглянутих фільмів?');
-const markOfFilm = prompt('Яка ваша оцінка?');
+for (let i = 0; i < 2; i++) {
+    let lastFilm = prompt('Який був ваш один з останніх переглянутих фільмів?');
+    let markOfFilm = prompt('Яка ваша оцінка?');
+    if( lastFilm == '' || lastFilm == null || lastFilm.length > 50 || 
+    markOfFilm == '' || markOfFilm == null || markOfFilm.length > 50) {
+        lastFilm = prompt('Який був ваш один з останніх переглянутих фільмів?');
+        markOfFilm = prompt('Яка ваша оцінка?');
+    } 
+    personalMovieDB.movies[lastFilm] = markOfFilm;
+}
 
-personalMovieDB.movies[lastFilm] = markOfFilm;
+if (personalMovieDB.count < 10) {
+    console.log('Переглянуто дуже мало фільмів');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <=30) {
+    console.log('Ви стандартний глядач');
+} else if (personalMovieDB.count > 30) {
+    console.log('Ви кіноман');
+} else {
+    console.log('Сталась помилка');
+}
 
+
+
+// let i = 0;
+// while (i < 2) {
+//     let lastFilm = prompt('Який був ваш один з останніх переглянутих фільмів?');
+//     let markOfFilm = prompt('Яка ваша оцінка?');
+//     if( lastFilm == '' || lastFilm == null || lastFilm.length > 50 || 
+//     markOfFilm == '' || markOfFilm == null || markOfFilm.length > 50) {
+//         lastFilm = prompt('Який був ваш один з останніх переглянутих фільмів?');
+//         markOfFilm = prompt('Яка ваша оцінка?');
+//     } 
+//     personalMovieDB.movies[lastFilm] = markOfFilm;
+//     i++;
+// }
+
+// let i = 0;
+// do {
+//     let lastFilm = prompt('Який був ваш один з останніх переглянутих фільмів?');
+//     let markOfFilm = prompt('Яка ваша оцінка?');
+//     if( lastFilm == '' || lastFilm == null || lastFilm.length > 50 || 
+//     markOfFilm == '' || markOfFilm == null || markOfFilm.length > 50) {
+//         lastFilm = prompt('Який був ваш один з останніх переглянутих фільмів?');
+//         markOfFilm = prompt('Яка ваша оцінка?');
+//     } 
+//     personalMovieDB.movies[lastFilm] = markOfFilm;
+//     i++;
+// } while (i < 2);
+
+console.log(personalMovieDB);
